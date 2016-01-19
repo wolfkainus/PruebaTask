7c1335dd72c32141b07499eb23c2ecb535c84e72
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2016-01-19 20:28:31 -0300

    Create references between project and task

---
+++ db/migrate/20160119232234_addtask_reftoproject.rb
@@ -0,0 +1,4 @@
+class AddtaskReftoproject < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119231610) do
+ActiveRecord::Schema.define(version: 20160119232234) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
